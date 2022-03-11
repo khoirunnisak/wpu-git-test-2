28b80eb89d842be7cf4b6cdf3ed617543678bd3a
khoirunnisak <khoirunnisak2100@gmail.com>
2022-03-11 22:39:35 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote Lagi</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam maiores officia dignissimos, corporis nobis recusandae voluptatum ea. Maiores dignissimos, cupiditate, tempore, molestias eveniet architecto cumque deleniti nisi ratione sunt quisquam?</p>
+</body>
+
+</html>
